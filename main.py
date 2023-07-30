ad6a1b35d2d3132f7a3fd0af479a1c04975ab35c
Mausmi Sinha <sinhatanu2001@gmail.com>
2023-07-30 17:36:16 +0530

    Facing error with Dataframe splitting

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('neo_v2.csv')
+# Loaded the Dataset
+data = pd.read_csv  ('neo_v2.csv')
 
+# Splitted the Dataset into Training and Testing
 Train, Test = train_test_split(data, test_size=0.3, random_state=0)
+
+train_labels = Train[:, 9]
+print(train_labels.head())
+
+Train.info()
 
 # Printing the Number of True and False Records in Train and Test Dataset
 print("Train Dataset: No of True: {}, No. False: {}".format(Train['hazardous'].sum(), len(Train) -Train['hazardous'].sum()))
