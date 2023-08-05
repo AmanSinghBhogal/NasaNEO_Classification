e20a579ec96c00143c674eefa5202f24f9f9fe21
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2023-08-05 15:40:55 +0530

    Restructed the data frame

--- main.py
+++ main.py
@@ -9,27 +9,22 @@ data = pd.read_csv  ('neo_v2.csv')
 data = data.drop("orbiting_body", axis = 1)
 data = data.drop("name", axis = 1)
 
-# Splitted the Dataset into Training and Testing
-Train, Test = train_test_split(data, test_size=0.3, random_state=0)
-
-'''
-The Scaler returns the NumPy-array instead of a Pandas DataFrame
-'''
+# The Scaler returns the NumPy-array instead of a Pandas DataFrame
 
 scaler = MinMaxScaler()
-scaler.fit(Train)
-scaler.fit(Test)
-Train = scaler.transform(Train)
-Test = scaler.transform(Test)
+scaler.fit(data)
+data = scaler.transform(data)
 
-# Creating train input data containing all attributes except the label attribute
-train_input = Train[:, 0:6]
-train_labels = Train[:, 7]
+input_data = data[:, 0:6]
+input_labels = data[:, 7]
 
-# Creating test input data containing all attributes except the label attribute
-test_input = Test[:, 0:6]
-test_labels = Test[:,7]
+# Splitted the Dataset into Training and Testing
+train_input, test_input, train_labels, test_labels = train_test_split(input_data, input_labels, test_size=0.3, random_state=0)
 
 # Printing the Number of True and False Records in Train and Test Dataset
 print("Train Dataset: No of True: {}, No. False: {}".format(train_labels.sum(), len(train_labels) -train_labels.sum()))
 print("Test Dataset: No of True: {}, No. False: {}".format(test_labels.sum(), len(test_labels) - test_labels.sum()))
+
+# Printing Number of records in training and testing
+print("There are {} Training Records and {} Testing Records".format(train_input.shape[0], test_input.shape[0]))
+print("There are {} input columns".format(train_input.shape[1]))
