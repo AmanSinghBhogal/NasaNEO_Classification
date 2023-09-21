3876b3368e6833b9b0cf8a1ab0840a0a5161f751
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2023-09-21 10:47:18 +0530

    Baysian Interface V1 Prepared

--- BaysianInterference.py
+++ BaysianInterference.py
@@ -443,3 +443,261 @@ def calculate_hazardous_params(objects):
     }
 
     return hazardous_params
+
+# Listing Prepare the data
+def prepare_data(objects, params):
+    """
+    params = {
+        'p_norm_large_slow_hazardous': 0.45,
+        'p_norm_large_slow_nonhazardous': 0.46,
+        'p_norm_large_fast_hazardous': 0.47,
+        'p_norm_large_fast_nonhazardous': 0.48,
+        'p_norm_small_slow_hazardous': 0.49,
+        'p_norm_small_slow_nonhazardous': 0.51,
+        'p_norm_small_fast_hazardous': 0.52,
+        'p_norm_small_fast_nonhazardous': 0.53,
+    }
+    """
+    # is the object large?
+    objects['IsLarge'] = objects['Categorized_Diameter'].map(lambda dia: 0 if dia == "Small" else 1)
+
+    # the probability of favored by norm given diameter, relative velocity, and hazardous
+    objects['Norm'] = list(map(
+        lambda item: params['p_norm_{}_{}_{}'.format(
+            'small' if item[0] == "Small" else 'large',
+            'slow' if item[1] == "Slow" else 'fast',
+            'nonhazardous' if item[2] == 0 else 'hazardous'
+        )],
+        list(zip(objects['IsLarge'], objects['Categorized_Relative_Vel'], objects['Hazardous']))
+    ))
+    return objects
+
+# Listing Initialize the parameters
+# Step 0: Initialize the parameter values 
+params = {
+    'p_norm_large_slow_hazardous': 0.45,
+    'p_norm_large_slow_nonhazardous': 0.46,
+    'p_norm_large_fast_hazardous': 0.47,
+    'p_norm_large_fast_nonhazardous': 0.48,
+    'p_norm_small_slow_hazardous': 0.49,
+    'p_norm_small_slow_nonhazardous': 0.51,
+    'p_norm_small_fast_hazardous': 0.52,
+    'p_norm_small_fast_nonhazardous': 0.53,
+}
+
+# Listing Run the qbn
+objects = prepare_data(train_input, params)
+results = qbn_neo(calculate_norm_params(objects), calculate_hazardous_params(objects), hist=False)
+
+# Listing Get a list of relevant states
+def filter_states(states, position, value):
+    return list(filter(lambda item: item[0][QUBITS-1-position] == str(value), states))
+
+# Listing The states with hazardous objects
+filter_states(results.items(), QPOS_hazardous, '1')
+
+# Listing Calculate the marginal probability to be hazardous
+def sum_states(states):
+    return sum(map(lambda item: item[1], states))
+
+sum_states(filter_states(results.items(), QPOS_hazardous, '1'))
+
+# Listing The log‐likelihood function adapted for our data
+def log_likelihood_neo(data, results):
+    states = results.items()
+    
+    def calc_prob(norm_val, islarge_val, rv_val, hazardous_val):
+        return sum_states(
+            filter_states(
+                filter_states(
+                    filter_states(
+                        filter_states(states, QPOS_RV, rv_val),
+                        QPOS_dia, islarge_val
+                    ), QPOS_hazardous, hazardous_val
+                ), QPOS_NORM, norm_val))
+        
+    probs = {
+        'p_favoured_large_slow_hazardous': calc_prob('1', '1', '0', '1'),
+        'p_favoured_large_slow_nonhazardous': calc_prob('1', '1', '0', '0'),
+        'p_favoured_large_fast_hazardous': calc_prob('1', '1', '1', '1'),
+        'p_favoured_large_fast_nonhazardous': calc_prob('1', '1', '1', '0'),
+        'p_favoured_small_slow_hazardous': calc_prob('1', '0', '0', '1'),
+        'p_favoured_small_slow_nonhazardous': calc_prob('1', '0', '0', '0'),
+        'p_favoured_small_fast_hazardous': calc_prob('1', '0', '1', '1'),
+        'p_favoured_small_fast_nonhazardous': calc_prob('1', '0', '1', '0'),
+        'p_unfavoured_large_slow_hazardous': calc_prob('0', '1', '0', '1'),
+        'p_unfavoured_large_slow_nonhazardous': calc_prob('0', '1', '0', '0'),
+        'p_unfavoured_large_fast_hazardous': calc_prob('0', '1', '1', '1'),
+        'p_unfavoured_large_fast_nonhazardous': calc_prob('0', '1', '1', '0'),
+        'p_unfavoured_small_slow_hazardous': calc_prob('0', '0', '0', '1'),
+        'p_unfavoured_small_slow_nonhazardous': calc_prob('0', '0', '0', '0'),
+        'p_unfavoured_small_fast_hazardous': calc_prob('0', '0', '1', '1'),
+        'p_unfavoured_small_fast_nonhazardous': calc_prob('0', '0', '1', '0'),
+    }
+
+    return round(sum(map(
+        lambda item: log(probs['p_{}_{}_{}_{}'.format(
+                'unfavoured',
+                'small' if item[1] == 0 else 'large',
+                'slow' if item[2] == "Slow" else 'fast',
+                'nonhazardous' if item[3] == 0 else 'hazardous'
+            )] + probs['p_{}_{}_{}_{}'.format(
+                'favoured',
+                'small' if item[1] == 0 else 'large',
+                'slow' if item[2] == "Slow" else 'fast',
+                'nonhazardous' if item[3] == 0 else 'hazardous'
+            )]
+        ),
+        list(zip(data['Norm'], data['IsLarge'], data['Categorized_Relative_Vel'], data['Hazardous']))
+    )), 3)
+
+# Listing Calculate the log‐likelihood
+log_likelihood_neo(train_input, results)
+
+# Listing Obtain new object values from the results
+def to_params(results):
+    states = results.items()
+    
+    def calc_norm(islarge_val, rv_val, hazardous_val):
+        pop = filter_states(filter_states(filter_states(states, QPOS_RV, rv_val), QPOS_dia, islarge_val), QPOS_hazardous, hazardous_val)
+
+        p_norm = sum(map(lambda item: item[1], filter_states(pop, QPOS_NORM, '1')))
+        p_total = sum(map(lambda item: item[1], pop))
+        return p_norm / p_total
+
+
+    return {
+        'p_norm_large_slow_hazardous': calc_norm('1', '0', '1'),
+        'p_norm_large_slow_nonhazardous': calc_norm('1', '0', '0'),
+        'p_norm_large_fast_hazardous': calc_norm('1', '1', '1'),
+        'p_norm_large_fast_nonhazardous': calc_norm('1', '1', '0'),
+        'p_norm_small_slow_hazardous': calc_norm('0', '0', '1'),
+        'p_norm_small_slow_nonhazardous': calc_norm('0', '0', '0'),
+        'p_norm_small_fast_hazardous': calc_norm('0', '1', '1'),
+        'p_norm_small_fast_nonhazardous': calc_norm('0', '1', '0'),
+    }
+
+# Listing Calcualte new objects
+to_params(results)
+
+# Listing The recursive training automatism
+def train_qbn_neo(objects, params, iterations):
+    if iterations > 0:
+        new_params = train_qbn_neo(objects, params, iterations - 1)
+
+        objects = prepare_data(objects, new_params)
+        results = qbn_neo(calculate_norm_params(objects), calculate_hazardous_params(objects), hist=False)
+
+        print ('The log-likelihood after {} iteration(s) is {}'.format(iterations, log_likelihood_neo(objects, results)))
+        return to_params(results)
+    
+    return params
+
+# Listing Train the QBN
+trained_params = train_qbn_neo(train_input, {
+    'p_norm_large_slow_hazardous': 0.45,
+    'p_norm_large_slow_nonhazardous': 0.46,
+    'p_norm_large_fast_hazardous': 0.47,
+    'p_norm_large_fast_nonhazardous': 0.48,
+    'p_norm_small_slow_hazardous': 0.49,
+    'p_norm_small_slow_nonhazardous': 0.51,
+    'p_norm_small_fast_hazardous': 0.52,
+    'p_norm_small_fast_nonhazardous': 0.53,
+}, 25)
+
+# Listing The parameters after training
+trained_params
+
+# Listing Pre‐processing
+def pre_process(object):
+    return (object['IsLarge'] == 1, object['Categorized_Relative_Vel'] == 'Fast', object['Categorised_Miss_Distance'])
+
+# Listing Apply the known data on the quantum circuit
+def apply_known(qc, is_large, is_fast, missDistance):
+    if is_large:
+        qc.x(QPOS_dia)
+
+    if is_fast:
+        qc.x(QPOS_RV)
+    
+    qc.x(QPOS_more if missDistance == "More" else (QPOS_med if missDistance == "Medium" else QPOS_less))
+
+# Listing Get the trained QBN
+def get_trained_qbn(objects, params):
+
+    prepared_objects = prepare_data(objects, params)
+    norm_params = calculate_norm_params(prepared_objects)
+    hazardous_params = calculate_hazardous_params(prepared_objects)
+
+    def trained_qbn_neo(object):
+        (is_large, is_fast, missDistance) = object
+
+        def circuit(qc, qr, cr):
+            apply_known(qc, is_large, is_fast, missDistance)
+            apply_norm(qc, norm_params)
+            apply_hazardous(qc, hazardous_params)
+            
+            qc.measure(qr[QPOS_hazardous], cr[0])
+        
+        return as_pqc(QUBITS, circuit, hist=False, measure=True, shots=100)
+
+    return trained_qbn_neo
+
+# Listing Post‐processing
+def post_process(counts):
+    """
+    counts -- the result of the quantum circuit execution
+    returns the prediction
+    """
+    #print (counts)
+    p_hazardous = counts['1'] if '1' in counts.keys() else 0
+    p_nonhazardous = counts['0'] if '0' in counts.keys() else 0
+
+    #return int(list(map(lambda item: item[0], counts.items()))[0])
+    return 1 if p_hazardous > p_nonhazardous else 0
+
+# Preparing Report
+def run(f_classify, x):
+    return list(map(f_classify, x))
+
+def specificity(matrix):
+    return matrix[0][0]/(matrix[0][0]+matrix[0][1]) if (matrix[0][0]+matrix[0][1] > 0) else 0
+
+def npv(matrix):
+    return matrix[0][0]/(matrix[0][0]+matrix[1][0]) if (matrix[0][0]+matrix[1][0] > 0) else 0
+
+def classifier_report(name, run, classify, input, labels):
+    cr_predictions = run(classify, input)
+    cr_cm = confusion_matrix(labels, cr_predictions)
+
+    cr_precision = precision_score(labels, cr_predictions)
+    cr_recall = recall_score(labels, cr_predictions)
+    cr_specificity = specificity(cr_cm)
+    cr_npv = npv(cr_cm)
+    cr_level = 0.25*(cr_precision + cr_recall + cr_specificity + cr_npv)
+
+    print('The precision score of the {} classifier is {:.2f}'
+        .format(name, cr_precision))
+    print('The recall score of the {} classifier is {:.2f}'
+        .format(name, cr_recall))
+    print('The specificity score of the {} classifier is {:.2f}'
+        .format(name, cr_specificity))
+    print('The npv score of the {} classifier is {:.2f}'
+        .format(name, cr_npv))
+    print('The information level is: {:.2f}'
+        .format(cr_level))
+    
+# Listing Run the Quantum Naive Bayes Classifier
+# redefine the run-function
+def run(f_classify, data):
+    return [f_classify(data.iloc[i]) for i in range(0,len(data))]
+
+# get the simple qbn
+trained_qbn = get_trained_qbn(train_input, trained_params)
+
+# evaluate the Quantum Bayesian Network
+classifier_report("QBN",
+    run,
+    lambda object: post_process(trained_qbn(pre_process(object))),
+    objects,
+    train_input['Hazardous'])
